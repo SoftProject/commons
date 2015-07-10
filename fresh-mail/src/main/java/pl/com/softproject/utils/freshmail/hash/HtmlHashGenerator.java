d7e2e7526c8b1441d9e7174b7e5719b6eb6e36ec
adrian <adrian@soft-project.pl>
2015-07-10 08:54:24 +0200

    - nieinwazyjny refactor - excludy w pom

--- fresh-mail/src/main/java/pl/com/softproject/utils/freshmail/hash/HtmlHashGenerator.java
+++ fresh-mail/src/main/java/pl/com/softproject/utils/freshmail/hash/HtmlHashGenerator.java
@@ -1,15 +1,11 @@
 package pl.com.softproject.utils.freshmail.hash;
 
-import org.apache.commons.codec.digest.DigestUtils;
-
-import java.io.Serializable;
-
 /**
  * Class HtmlHashGenerator
  *
  * @author Marcin Jasiński {@literal <mkjasinski@gmail.com>}
  */
-public class HtmlHashGenerator implements Serializable, HashGenerator {
+public class HtmlHashGenerator extends AbstractHashGenerator {
 
     private static final String CONTENT_TYPE = "text/json";
 
@@ -18,8 +14,5 @@ public class HtmlHashGenerator implements Serializable, HashGenerator {
         return CONTENT_TYPE;
     }
 
-    @Override
-    public String generate(String apiKey, String getData, String postData, String apiSecret) {
-        return DigestUtils.sha1Hex(apiKey + getData + postData + apiSecret);
-    }
+
 }
--- fresh-mail/src/main/java/pl/com/softproject/utils/freshmail/hash/JsonHashGenerator.java
+++ fresh-mail/src/main/java/pl/com/softproject/utils/freshmail/hash/JsonHashGenerator.java
@@ -1,15 +1,11 @@
 package pl.com.softproject.utils.freshmail.hash;
 
-import org.apache.commons.codec.digest.DigestUtils;
-
-import java.io.Serializable;
-
 /**
  * Class JsonHashGenerator
  *
  * @author Marcin Jasiński {@literal <mkjasinski@gmail.com>}
  */
-public class JsonHashGenerator implements Serializable, HashGenerator {
+public class JsonHashGenerator extends AbstractHashGenerator {
 
     private static final String CONTENT_TYPE = "application/json";
 
@@ -18,8 +14,4 @@ public class JsonHashGenerator implements Serializable, HashGenerator {
         return CONTENT_TYPE;
     }
 
-    @Override
-    public String generate(String apiKey, String getData, String postData, String apiSecret) {
-        return DigestUtils.sha1Hex(apiKey + getData + postData + apiSecret);
-    }
 }
